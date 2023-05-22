39447a5d45d0d4ed6a34838c2fe14fa2e591c237
jju6924@naver.com <jju6924@naver.com>
2023-05-22 15:21:07 +0900

    MovieList 화제작 부분

--- final-pjt-front/src/views/movies/MovieList.vue
+++ final-pjt-front/src/views/movies/MovieList.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-    <b-container class="mb-5">
-      <b-row class="d-flex justify-content-center">
-        <h1 class="font-do my-3">화제작</h1>
-      </b-row>
+    <div class="mb-5" style="max-width: 370px; margin-left: auto; margin-right: 5%">
+      <h1 class="font-do my-3"></h1>
+      <b-row class="d-flex justify-content-end"> </b-row>
       <b-row>
         <b-col class="d-flex justify-content-end">
           <b-carousel
@@ -17,6 +16,8 @@
             @sliding-start="onSlideStart"
             @sliding-end="onSlideEnd"
             class="font-poor"
+            ref="carouselRef"
+            fade
           >
             <b-carousel-slide
               v-for="(movie, idx) in ordered"
@@ -24,23 +25,25 @@
               :caption="movie.title"
               img-blank-color="dark"
               img-height="480"
-              img-width="320"
+              style="width: auto"
             >
               <template #img>
-                <img
-                  class="d-block img-fluid"
-                  :src="`https://image.tmdb.org/t/p/w185${movie.poster_path}`"
-                  alt="image slot"
-                />
+                <div style="display: flex; justify-content: flex-end">
+                  <img
+                    class="d-block img-fluid"
+                    :src="`https://image.tmdb.org/t/p/w185${movie.poster_path}`"
+                    alt="image slot"
+                    style="max-height: 480px"
+                  />
+                </div>
               </template>
-              <!-- <p>{{ movie.overview | truncate(300, '...') }}</p> -->
             </b-carousel-slide>
           </b-carousel>
         </b-col>
       </b-row>
-    </b-container>
+    </div>
 
-    <div class="container" v-if="login && recommend_list.length > 0">
+    <div class="container mt-5" v-if="login && recommend_list.length > 0">
       <h1 class="font-do my-3">당신을 위한 영화 추천!</h1>
       <div class="row">
         <MovieListItem v-for="(movie, idx) in recommend_list" :key="idx" :movie="movie" class="col-3" />
@@ -51,10 +54,6 @@
       <h1 class="font-do my-5">영화 리스트</h1>
     </div>
     <div class="container">
-      <!-- <div class="row justify-content-end mb-4">
-        <label class="h2 mr-3" for="">Search: </label>
-        <input style="width: 300px;" class="form-control" type="text" v-model.trim='search'>
-      </div> -->
       <div class="row">
         <MovieListItem v-for="(movie, idx) in movies" :key="idx" :movie="movie" class="col-3" :search="search" />
       </div>
@@ -95,6 +94,19 @@ export default {
     onSlideEnd() {
       this.sliding = false;
     },
+    // resetCarouselHeight() {
+    //   const carousel = this.$refs.carouselRef;
+    //   if (carousel) {
+    //     const images = carousel.$el.querySelectorAll('.d-block.img-fluid');
+    //     if (images) {
+    //       let maxHeight = 0;
+    //       images.forEach((image) => {
+    //         maxHeight = Math.max(maxHeight, image.offsetHeight);
+    //       });
+    //       carousel.$el.style.height = `${maxHeight}px`;
+    //     }
+    //   }
+    // },
   },
   created: function () {
     this.i = 0;
@@ -126,6 +138,13 @@ export default {
     if (this.login === true && this.is_admin === false) {
       this.$store.dispatch('recommendMovie');
     }
+    window.addEventListener('resize', this.resetCarouselHeight);
+    this.$nextTick(() => {
+      this.resetCarouselHeight();
+    });
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.resetCarouselHeight);
   },
 };
 </script>
